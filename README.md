6e85545c94df3ae1e38b777a5f95d7c6d01ff11c
AlejandroParraTorres <aparrato99@gmail.com>
2023-04-22 12:13:45 +0200

    Estructura del juego

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Juego-HTML-CSS-JavaScript
+# TrainYourMind
+
+Juego de preguntas y respuestas donde tendrás un tiempo limitado para responder X preguntas.
+
+. Cada pregunta respondida correctamente sumara +3 puntos
+. Cada pregunta respondida incorrectamente restara -1 punto
+
+El objetivo del juego es lograr la mayor puntuacion posible en el tiempo que se mostrará arriba.
+
+Existe la posiblidad de que durante el juego existan perks para poder ayudarte a responder las preguntas. Estas habilidades se conseguiran comprando en una futura tienda con el dinero que se conseguira una vez se termine el juego. Todo el progreso y los intentos asi como el dinero recogido se guardara en el LocalStorage.
---
+++ myscript.js
@@ -0,0 +1,110 @@
+
+
+
+
+//INICIO CODIGO DEL JUEGO
+
+const preguntas = [
+  {
+    question: "¿Cuál es la capital de Eritrea?",
+    options: ["Asmara", "Yamena", "Kigali", "Lomé", "Bamako"],
+    answer: "Asmara"
+  },
+  {
+    question: "¿En qué año comenzó la Guerra de los Cien Años?",
+    options: ["1300", "1337", "1376", "1415", "1450"],
+    answer: "1337"
+  },
+  {
+    question: "¿En qué año murió el famoso científico Stephen Hawking?",
+    options: ["2014", "2016", "2018", "2020", "2022"],
+    answer: "2018"
+  },
+  {
+    question: "¿Cuál es el nombre del fenómeno meteorológico que ocurre en el Pacífico tropical?",
+    options: ["El Niño", "La Niña", "El Viento Sur", "La Brisa Marítima", "La Corriente Fría"],
+    answer: "El Niño"
+  },
+  {
+    question: "¿En qué año se estrenó la película de ciencia ficción Blade Runner?",
+    options: ["1980", "1982", "1984", "1986", "1988"],
+    answer: "1982"
+  },
+  {
+    question: "¿Quién escribió la novela El Código Da Vinci?",
+    options: ["Dan Brown", "Tom Clancy", "John Grisham", "Michael Crichton", "Ken Follett"],
+    answer: "Dan Brown"
+  },
+  {
+    question: "¿Cuál es el nombre de la obra cumbre del escritor James Joyce?",
+    options: ["Ulises", "Retrato del Artista Adolescente", "Dublineses", "Finnegans Wake", "Exiliados"],
+    answer: "Ulises"
+  },
+  {
+    question: "¿Cuál es el nombre del satélite natural de Urano?",
+    options: ["Io", "Titán", "Ganimedes", "Tritón", "Miranda"],
+    answer: "Miranda"
+  },
+  {
+    question: "¿En qué año se fundó la compañía de tecnología Apple?",
+    options: ["1972", "1976", "1980", "1984", "1988"],
+    answer: "1976"
+  },
+  {
+    question: "¿En qué año se proclamó la independencia de Brasil?",
+    options: ["1808", "1822", "1844", "1860", "1888"],
+    answer: "1822"
+  },
+  {
+    question: "¿En qué año se estrenó la película The Shawshank Redemption?",
+    options: ["1992", "1994", "1996", "1998", "2000"],
+    answer: "1994"
+  },
+  {
+    question: "¿En qué país se encuentra la ciudad de Fez?",
+    options: ["Marruecos", "Túnez", "Egipto", "Argelia", "Libia"],
+    answer: "Marruecos"
+  },
+  {
+    question: "¿Quién es el autor de la novela Moby Dick?",
+    options: ["Herman Melville", "Mark Twain", "Charles Dickens", "F. Scott Fitzgerald", "Ernest Hemingway"],
+    answer: "Herman Melville"
+  },
+  {
+    question: "¿Qué país tiene la forma de una bota?",
+    options: ["España", "Italia", "Grecia", "Portugal", "Francia"],
+    answer: "Italia"
+  },
+  {
+    question: "¿Cuál es el nombre del fundador de la filosofía estoica?",
+    options: ["Epicteto", "Séneca", "Marco Aurelio", "Zenón de Citio", "Cicerón"],
+    answer: "Zenón de Citio"
+  },
+  {
+    question: "¿Qué escritor colombiano ganó el Premio Nobel de Literatura en 1982?",
+    options: ["Gabriel García Márquez", "Mario Vargas Llosa", "Octavio Paz", "Pablo Neruda", "Jorge Luis Borges"],
+    answer: "Gabriel García Márquez"
+  },
+  {
+    question: "¿Cuál es el nombre del inventor de la bombilla eléctrica?",
+    options: ["Alexander Graham Bell", "Thomas Edison", "Nikola Tesla", "Guglielmo Marconi", "Benjamin Franklin"],
+    answer: "Thomas Edison"
+  },
+  {
+    question: "¿En qué país se encuentra la isla de Tasmania?",
+    options: ["Australia", "Nueva Zelanda", "Indonesia", "Papúa Nueva Guinea", "Fiyi"],
+    answer: "Australia"
+  },
+  {
+    question: "¿Quién pintó el famoso cuadro La Noche Estrellada?",
+    options: ["Vincent van Gogh", "Pablo Picasso", "Leonardo da Vinci", "Johannes Vermeer", "Rembrandt van Rijn"],
+    answer: "Vincent van Gogh"
+  },
+  {
+    question: "¿Cuál es el nombre del primer presidente de Estados Unidos?",
+    options: ["George Washington", "John Adams", "Thomas Jefferson", "Abraham Lincoln", "Franklin D. Roosevelt"],
+    answer: "George Washington"
+  }
+]
+
+
